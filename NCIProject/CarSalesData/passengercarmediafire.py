bc4b306e31671bb9cb52cee40c1a9c98aa57e207
ATGITHUB94 <47369013+ATGITHUB94@users.noreply.github.com>
2019-12-04 21:21:29 +0000

    Add files via upload

---
+++ NCIProject/CarSalesData/passengercarmediafire.py
@@ -0,0 +1,5 @@
+import wget
+url = "https://download2265.mediafire.com/dmfzjn4ma9kg/c1spuwklgzavig1/passengercars.csv"
+wget.download(url, 'C:\\Users\\alain\\Documents\\NCI\\1st semester\\Database and analytics\\project\\passengercarmediafire\\passengercars.csv')
+
+#https://www.mediafire.com/file/wr4hgtwmqej0bxe/passengercars.csv/file
