bcfa335f052a60794a89baaf6b110d474e302bcd
oreillydavid7 <55714378+oreillydavid7@users.noreply.github.com>
2019-12-07 14:13:39 +0000

    added the code to project

--- NCIProject/ChargingPointData/CA - Extract Zip files.py
+++ NCIProject/ChargingPointData/CA - Extract Zip files.py
@@ -37,14 +37,14 @@ soup = BeautifulSoup(response.content, "html.parser")
 #loop goes from a range being  #3 a ref link to the last a link ref (Start at 0), 
 #each iteration +1 to the range, therefore traversing to the next link
 
-for i in range(2,len(soup.findAll('a'))+1): #'a' tags are for links
+for i in range(2,len(soup.findAll('a'))+1): #'a' tags are for links  
     #while i != 0: #while the a_zip has a value not null, go through the loop'''
     a_zip = soup.findAll('a')[i] #after the 2nd a, the next a tag is the link name 
     #print("a_zip: ", a_zip) # test to ensure each a ref in the loop is collected...
     ziplink = a_zip['href'] # convert the a ref to an actual web link    
     pass_ziplink = requests.get(ziplink)
     #urllib.request.urlretrieve(ziplink) 
-    print("Name of zip Exported: ", ziplink) # test to ensure each a ref in the loop is collected...
+    print("Name of zip Exported: ", ziplink) # test to ensure each a ref in the loop is collected.....
     zip = zipfile.ZipFile(io.BytesIO(pass_ziplink.content)) #zipfile library to extract source data
     zip.extractall(path) #extract to specified location...should this be Github? 
     '''Next steps: with the downloaded file, transfer to SQL or equivalent - ETL applied here
